87034258052ce13dc3ca88be6f54af58544ae8c6
SabirovTimur <sabirovtimurcfc@gmail.com>
2023-06-14 18:48:29 +0300

    Добавлены тесты

---
+++ src/test/java/ru/netology/tests/Test.java
@@ -0,0 +1,83 @@
+package ru.netology.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.Data.Registration.getRegisteredUser;
+import static ru.netology.data.Data.Registration.getUser;
+import static ru.netology.data.Data.getRandomLogin;
+import static ru.netology.data.Data.getRandomPassword;
+
+public class Test {
+    @BeforeEach
+    public void setup() {
+        open("http://localhost:9999");}
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Should successfully login with active registered user")
+    public void testSuccessfulLoginIfRegisteredUserIsActive() {
+        Configuration.holdBrowserOpen = true;
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("h2")
+                .shouldHave(Condition.exactText("Личный кабинет"))
+                .shouldBe(Condition.visible);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Should get error massage if login with not registered user")
+    public void testGetErrorIfUserIsNotRegistered() {
+        var notRegisteredUser= getUser("active");
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe((Condition.visible));
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Should get error massage if login with blocked registered user")
+    public void testGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Пользователь заблокирован"))
+                .shouldBe((Condition.visible));
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Should get error if login with wrong login")
+    public void testGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe((Condition.visible));
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Should set error if password is wrong")
+    public void testGetErrorIfPasswordWrong() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe((Condition.visible));
+
+    }
+}
